35753f6885d374bed517a361d3627a5a2e511158
tien0510 <1851061636@e.tlu.edu.vn>
2021-08-27 02:59:28 +0700

    add file contact.php

---
+++ contact.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/9a1a0cef63.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/contact.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php require_once('header.php') ?>
+    <div class="contact">
+    <div class="map">
+        <div class="contact-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7450.229442217748!2d105.94426!3d20.988038!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x136290b61ba411db!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBWaW5Vbml2ZXJzaXR5!5e0!3m2!1svi!2sus!4v1629996158028!5m2!1svi!2sus" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <section class="main contact">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="contact-col">
+                       <div class="main-contact">
+                           <div class="section-title">
+                               <h2>Connect with us</h2>
+                               <div class="pc1">
+                                You can stay connected with us by signing up to receive emails, following VinUniversity on our Facebook fanpage, or connecting with us on LinkedIn.
+                               </div>
+                           </div>
+                           <form action="">
+                               <input type="text" placeholder="Your name">
+                               <input type="text" placeholder="Phone Number">
+                               <input type="email" placeholder="Email">
+                               <input class="content-input" type="text" placeholder="What do you want to know ">
+                               <p>
+                                By submitting this request you are agreeing to be contacted by VinUniversity and being included on a mailing/contact list. We will never sell or disclose your information to a third party without your prior permission.
+                               </p>
+                               <input type="submit" value="Submit">
+                           </form>
+                       </div>
+                    </div>
+                    <div class="contact-col">
+                        <div class="block-info">
+                            <ul class="info">
+                                <li class="item">
+                                    
+                                        <div class="fontawe">
+                                            <i class="fas fa-map-marker-alt"></i>
+                                        </div>
+                                        <div>
+                                            <h4>
+                                                Vinuniversity
+                                            </h4>
+                                            <span>
+                                                Vinhomes Ocean Park, Gia Lam <br>
+                                                District, Hanoi.
+                                            </span>
+                                        </div>
+                                    
+                                </li>
+                                <li class="item">
+                                    
+                                        <div class="fontawe">
+                                            <i class="fas fa-phone"></i>
+                                        </div>
+                                        <div>
+                                            <h4>
+                                                Phone :
+                                            </h4>
+                                            <span>
+                                                <a href="tel:+84-2471089779 or 18008189">+84-2471089779 or 18008189</a>
+                                            </span>
+                                        </div>
+                                    
+                                </li>
+                                <li class="item">
+                                    
+                                        <div class="fontawe">
+                                            <i class="fas fa-envelope"></i>
+                                        </div>
+                                        <div>
+                                            <h4>
+                                               Email :
+                                            </h4>
+                                            <span>
+                                                <a href="mailto:info@vinuni.edu.vn">info@vinuni.edu.vn</a>
+                                            </span>
+                                        </div>
+                                    
+                                </li>
+                                <li class="item">
+                                    
+                                        <div class="fontawe">
+                                            <i class="fab fa-facebook-f"></i>
+                                        </div>
+                                        <div>
+                                            <h4>
+                                                Fanpage :
+                                            </h4>
+                                            <span>
+                                                <a href="https://www.facebook.com/nammmm.mee/">@VinUniversity</a>
+                                            </span>
+                                        </div>
+                                    
+                                </li>
+                                <li class="item">
+                                    
+                                        <div class="fontawe">
+                                            <i class="fab fa-linkedin-in"></i>
+                                        </div>
+                                        <div>
+                                            <h4>
+                                                Linkedin:
+                                            </h4>
+                                            <span>
+                                                <a href="">@VinUniversity</a>
+                                            </span>
+                                        </div>
+                                    
+                                </li>
+                            </ul>
+
+                        </div>
+                        <div class="aa">
+                            <div class="img">
+                                <img src="images/DSC00955-scaled-e1589900169583.jpg" alt="">
+                            </div>
+                        </div>
+                        
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </section>
+    </div>
+    <div class="clearfix"></div>
+        <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="css/footer.css">
+<footer id="footer" class="footer-1">
+  <div class="main-footer widgets-dark typo-light">
+  <div class="row" style="    display: flex;
+    flex-wrap: nowrap; " >
+    
+  <div class="col-xs-12 col-sm-6 col-md-3">
+  <div class="widget subscribe no-box" style="margin-left: 30px ;display :flex;">
+  <h5 class="widget-title"><a class="logo" href="https://vinuni.edu.vn/">
+                    <img src="https://vinuni.edu.vn/wp-content/themes/vinuni/assets/images/logo/logo-white.svg" alt="footer logo" width="164" height="100"></a></h5>
+   </div>
+  </div>
+  <div class="col-xs-12 col-sm-6 col-md-3">
+  <div class="widget no-box"style="margin-left: 100px">
+  <h5 class="widget-title">Schools, Programs Office<span></span></h5>
+  <ul class="thumbnail-widget">
+  <li><div class="thumb-content"><a href="#.">College of Business & Management</a></div>   </li>
+  <li><div class="thumb-content"><a href="#.">College of Engineering & Computer </a></div>   </li>
+  <li><div class="thumb-content"><a href="#.">College of Health Sciences</a></div>   </li>
+  <li><div class="thumb-content"><a href="#.">Faculty of Arts and Sciences</a></div> </li>
+  <li><div class="thumb-content"><a href="#.">Academic Calendar</a></div>  </li>
+  <li><div class="thumb-content"><a href="#.">Research</a></div>  </li>
+  </ul>
+  </div>
+  </div>
+  <div class="col-xs-12 col-sm-6 col-md-3">
+  <div class="widget no-box" style="margin-left: 100px">
+  <h5 class="widget-title">Admissions & Aid<span></span></h5>
+  
+  <ul class="thumbnail-widget">
+  <li><div class="thumb-content"><a href="#.">Undergraduate</a></div>   </li>
+  <li><div class="thumb-content"><a href="#.">Postgraduate</a></div>   </li>
+  <li><div class="thumb-content"><a href="#.">Overseas Master/PhD Scholarships</a></div>   </li>
+
+
+  </ul>
+       
+  </div>
+  </div>
+
+  <div class="col-xs-12 col-sm-6 col-md-3">
+
+  <div class="widget no-box" style="margin-left: 100px">
+  <h5 class="widget-title">FAQ<span></span></h5>
+
+  <ul class="thumbnail-widget">
+  <li><div class="thumb-content"><a href="#.">Campus Map</a></div>   </li>
+  <li><div class="thumb-content"><a href="#.">Contact Us</a></div>   </li>
+  <li><div class="thumb-content"><a href="#.">Library</a></div>   </li>
+
+
+  </ul>
+  
+  </div>
+  </div>
+  </div>
+  </div>  
+   <div class="footer-copyright">
+  </div>
+
+
+
+
+<div class="foot" >
+        <div class="foot_top" id="foot_top" >
+
+            <div class="foot_left">
+                <ul><p>
+                    Copyright © 2019 VinUni. All Right Reserved. Privacy Policy
+                </p>
+                </ul>
+            </div>
+            <div class="foot_right" style="margin-top: -85px;">
+                
+            </div>
+
+        </div>
+</div>
+
+
+
+
+
+
+
+
+  </footer>                                 
+
+    
+</body>
+</html>
