d7fe2e2d236c7cffca98bccb3c4542bac0e8e6d6
tien0510 <1851061636@e.tlu.edu.vn>
2021-08-30 01:21:59 +0700

    small update

--- research.php
+++ research.php
@@ -23,9 +23,21 @@
 
         $sql = "select * from category where name_category = 'research' ";
         $research = select_one($sql);
-        if ($research != null) {?>
+        if ($research != null) {
+            $a='../../';
+          $b=" ".$research["thumbnail"];
+          if(strpos($b,$a)){ //  kiểm tra a có trong b không
+            $research["thumbnail"] = str_replace('../../images','images' ,$research["thumbnail"]);
+           }
+          else{
+               $research["thumbnail"] = $research["thumbnail"] ;
+         
+          }
+          
+          $thumbnail   = $research['thumbnail']; 
+            ?>
              <div class="view">
-        <img class="d-block w-100" src="<?=$research['thumbnail']?>">
+        <img class="d-block w-100" src="<?=$thumbnail?>">
 
         </div>
 
@@ -38,7 +50,6 @@
          $sql = "select * from post where id_category = " .$research['id'] ;
         $list = select_list($sql);
         foreach($list as $data) {?>
-
         <div class="content">
             <div class="grid-container">
                 <div class="grid-item">
@@ -57,62 +68,6 @@
       <?php  }   }  ?>
         <br><br>
 
-
-
-        
-        <div class="end">
-            <div class="grid-end">
-                <div class="end-item">
-                    <div class="end-img">
-                        <img src="https://vinuni.edu.vn/wp-content/uploads/2020/05/pexels-photo-136419-e1592720188140-660x410.jpeg" alt="">
-                    </div>
-                    <div class="end-content">
-                        <h2>
-                            College of Business & Management
-                        </h2>
-                        <br>
-                        <p>
-                            The CBM faculty at VinUniversity is committed to conducting world-class research that is both rigorous and relevant. We aim to provide answers to practical
-                            real-world problems that can shape economic policy and influence the way business is done in Vietnam and around the world.
-                        </p>
-                        <a href="">Read morre</a>
-                    </div>
-                </div>
-                <div class="end-item">
-                    <div class="end-img">
-                        <img src="https://vinuni.edu.vn/wp-content/uploads/2020/05/pexels-photo-136419-e1592720188140-660x410.jpeg" alt="">
-                    </div>
-                    <div class="end-content">
-                        <h2>
-                            College of Business & Management
-                        </h2>
-                        <br>
-                        <p>
-                            The CBM faculty at VinUniversity is committed to conducting world-class research that is both rigorous and relevant. We aim to provide answers to practical
-                            real-world problems that can shape economic policy and influence the way business is done in Vietnam and around the world.
-                        </p>
-                        <a href="">Read morre</a>
-                    </div>
-                </div>
-
-                <div class="end-item">
-                    <div class="end-img">
-                        <img src="https://vinuni.edu.vn/wp-content/uploads/2020/05/pexels-photo-136419-e1592720188140-660x410.jpeg" alt="">
-                    </div>
-                    <div class="end-content">
-                        <h2>
-                            College of Business & Management
-                        </h2>
-                        <br>
-                        <p>
-                            The CBM faculty at VinUniversity is committed to conducting world-class research that is both rigorous and relevant. We aim to provide answers to practical
-                            real-world problems that can shape economic policy and influence the way business is done in Vietnam and around the world.
-                        </p>
-                        <a href="">Read morre</a>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
         <?php
   require_once('footer.php');
